8937db15cb9878e058b02d4cd5d1dbc871172613
Dylan Seto <dylan.seto@outlook.com>
2021-09-11 19:05:08 -0400

    Added Comments to build script

--- buildContracts.bat
+++ buildContracts.bat
@@ -1,8 +1,11 @@
+REM Clean Teal Scripts
 cd ./build
 del *.teal
 cd ..
 
+REM Build Smart Contracts from Python to Teal
 python ./src/contract/escrow_account.py >> ./build/escrow_account.teal
 
+REM Compile & Deploy TEAL Smart Contracts
 python ./src/deploy.py
 pause
