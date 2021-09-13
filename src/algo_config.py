42d07294d7129095df98d5af04bc15754357dfe5
Dylan Seto <dylan.seto@outlook.com>
2021-09-13 07:41:03 -0400

    Adds post creation to the application SC

--- src/algo_config.py
+++ src/algo_config.py
@@ -3,4 +3,4 @@ algod_token = "SxyeYnXjIi7sydMnmi85L8mqXypdroBv1ZdTcBmp"
 headers = {
     "X-API-Key": algod_token,
 }
-m_mnemonic = "horse slow toward twelve win kiwi delay keen steak survey second syrup sponsor stumble favorite below physical indoor nominee area team desert current abstract weather"
+m_mnemonic = "employ fly ahead odor finger olympic virtual suffer sugar fold hand shop latin fortune elephant valve timber notable oblige screen pioneer tiger snake abandon visit"
--- src/contract/createPost.py
+++ src/contract/createPost.py
@@ -8,11 +8,23 @@ def createPost():
         App.globalPut(Bytes('escrow'), Txn.application_args[1]),
         Int(1)
     ])
+
+    on_create_post = Seq([
+                    Assert(Global.group_size() == Int(2)), # Check that this is a group transaction.
+                    Assert(Gtxn[1].sender() == App.globalGet(Bytes('escrow'))), # Check that the sender is the escrow account.
+                    Assert(Gtxn[1].type_enum() == TxnType.AssetConfig),
+                    Assert(Eq(Gtxn[1].config_asset_name(), Bytes("asa_post"))), # Check Asset name.
+                    Assert(Gtxn[1].config_asset_default_frozen() == Int(1)), # Check frozen
+                    Assert(Gtxn[1].config_asset_total() == Int(1)), # That supply == 1 (NFT)
+                    Assert(Len(Gtxn[1].config_asset_metadata_hash()) > Int(0)), # Metadata is not empty
+                    Int(1)
+    ])
     
     program = Cond( 
         # On app creation
         [Txn.application_id() == Int(0), Int(1)],
-        [Txn.application_args[0] == Bytes("set_escrow"), on_set_escrow]
+        [Txn.application_args[0] == Bytes("set_escrow"), on_set_escrow],
+        [Txn.application_args[0] == Bytes("create_post"), on_create_post]
         )
         
     return And(Txn.group_index() == Int(0), program)
